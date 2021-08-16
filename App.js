2f48ab3f7115b26c99548f90fd061910dd0d0901
Giovani Alves <giovani.alves121@gmail.com>
2021-08-16 03:24:25 -0300

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,146 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
-export default function App() {
+import {
+  StatusBar,
+  FlatList,
+  Image,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+const { width, height } = Dimensions.get("screen");
+
+const API_KEY = "563492ad6f9170000100000158775d443842496a9dc83c9ced26731f";
+const API_URL =
+  "https://api.pexels.com/v1/search?query=nature&orientation=portrait&size=small&per_page=20";
+
+const IMAGE_SIZE = 80;
+const SPACING = 12;
+
+const fetchImagesFromPexels = async () => {
+  const data = await axios.get(API_URL, {
+    headers: {
+      Authorization: API_KEY,
+    },
+  });
+
+  const { photos } = data.data;
+
+  return photos;
+};
+
+const App = () => {
+  const [images, setImages] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const images = await fetchImagesFromPexels();
+      setImages(images);
+    };
+
+    fetchImages();
+  }, []);
+
+  const topRef = useRef();
+  const thumbRef = useRef();
+
+  const scrollToActiveIndex = (index) => {
+    setActiveIndex(index);
+
+    topRef?.current?.scrollToOffset({
+      offset: index * width,
+      animated: true,
+    });
+
+    if (index * (IMAGE_SIZE + SPACING) - IMAGE_SIZE / 2 > width / 2) {
+      thumbRef?.current?.scrollToOffset({
+        offset: index * (IMAGE_SIZE + SPACING) - width / 2 + IMAGE_SIZE / 2,
+        animated: true,
+      });
+    } else {
+      thumbRef?.current?.scrollToOffset({
+        offset: 0,
+        animated: true,
+      });
+    }
+  };
+
+  if (!images) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#000",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "#FFF" }}>Loading...</Text>
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#000" }}>
+      <FlatList
+        ref={topRef}
+        data={images}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onMomentumScrollEnd={(event) => {
+          scrollToActiveIndex(
+            Math.floor(event.nativeEvent.contentOffset.x / width)
+          );
+        }}
+        keyExtractor={(image) => image.id}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ width, height }}>
+              <Image
+                source={{ uri: item.src.portrait }}
+                style={[StyleSheet.absoluteFillObject]}
+              />
+            </View>
+          );
+        }}
+      />
+      <FlatList
+        ref={thumbRef}
+        data={images}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(image) => image.id}
+        contentContainerStyle={{ padding: SPACING }}
+        style={{
+          position: "absolute",
+          bottom: IMAGE_SIZE,
+        }}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableOpacity onPress={() => scrollToActiveIndex(index)}>
+              <Image
+                source={{ uri: item.src.portrait }}
+                style={{
+                  width: IMAGE_SIZE,
+                  height: IMAGE_SIZE,
+                  borderRadius: 12,
+                  marginRight: SPACING,
+                  borderWidth: 2,
+                  borderColor: activeIndex === index ? "#FFF" : "transparent",
+                }}
+              />
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <StatusBar hidden />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
